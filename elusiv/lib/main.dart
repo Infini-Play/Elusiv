ee1e0148241a9dae4b72758b84f13377d5bd914a
K3LV0N <kylenorman44@gmail.com>
2024-10-15 16:17:57 -0400

    Changed main file to remove old backend

--- elusiv/lib/main.dart
+++ elusiv/lib/main.dart
@@ -1,13 +1,40 @@
 import 'package:elusiv/core/navigation/routing.dart';
 import 'package:elusiv/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+
+Future<void> main() async {
+  // // Determine the environment and load the corresponding .env file
+  // const String env = String.fromEnvironment('FLAVOR', defaultValue: 'local');
+  // String envFile;
+  // switch (env) {
+  //   case 'staging':
+  //     envFile = '.env.staging';
+  //     break;
+  //   case 'prod':
+  //     envFile = '.env.prod';
+  //     break;
+  //   case 'local':
+  //   default:
+  //     envFile = '.env.local';
+  //     break;
+  // }
+  // // Load the environment variables
+  // await dotenv.load(fileName: envFile);
+  // // Initialize Supabase
+  // await Supabase.initialize(
+  //   url: dotenv.env['SUPABASE_URL']!,
+  //   anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  //   // Optionally, include other Supabase options
+  // );
+
+  runApp(const Elusiv());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Elusiv extends StatelessWidget {
+  const Elusiv({super.key});
 
   // This widget is the root of your application.
   @override
