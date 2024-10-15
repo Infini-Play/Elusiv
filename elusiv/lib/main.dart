145b80f33767887335404bb4fed86756e1a7f589
Maksim P <maksim.peter777@gmail.com>
2024-10-15 17:33:43 -0400

    Transition from supabase to pocketbase. Add AuthStore stored in SharedPreferences

--- elusiv/lib/main.dart
+++ elusiv/lib/main.dart
@@ -1,36 +1,55 @@
 import 'package:elusiv/core/navigation/routing.dart';
 import 'package:elusiv/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'package:pocketbase/pocketbase.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
-  // // Determine the environment and load the corresponding .env file
-  // const String env = String.fromEnvironment('FLAVOR', defaultValue: 'local');
-  // String envFile;
-  // switch (env) {
-  //   case 'staging':
-  //     envFile = '.env.staging';
-  //     break;
-  //   case 'prod':
-  //     envFile = '.env.prod';
-  //     break;
-  //   case 'local':
-  //   default:
-  //     envFile = '.env.local';
-  //     break;
-  // }
-  // // Load the environment variables
-  // await dotenv.load(fileName: envFile);
-  // // Initialize Supabase
-  // await Supabase.initialize(
-  //   url: dotenv.env['SUPABASE_URL']!,
-  //   anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
-  //   // Optionally, include other Supabase options
-  // );
+  // Determine the environment and load the corresponding .env file
+  const String env = String.fromEnvironment('FLAVOR', defaultValue: 'local');
+  String apiURl;
+  switch (env) {
+    case 'staging':
+      apiURl = 'https://stagingelusiv.infiniplay.games/';
+      break;
+    case 'prod':
+      apiURl = 'https://elusiv.infiniplay.games/';
+      break;
+    case 'local':
+    default:
+      apiURl = 'http://127.0.0.1:8090';
+      break;
+  }
 
-  runApp(const Elusiv());
+  // create authstore to store credentials and retrieve stored credentials
+
+  //create SharedPreferences instance
+  final prefs = await SharedPreferences.getInstance();
+
+  //create AsyncAuthStore instance
+  final store = AsyncAuthStore(
+    save: (String data) async => prefs.setString('pb_auth', data),
+    initial: prefs.getString('pb_auth'),
+  );
+
+  // Initialize PocketBase with the API URL
+  final pb = PocketBase(apiURl, authStore: store);
+
+  // get pb instance in any widget using provider
+  //import 'package:pocketbase/pocketbase.dart';
+  //import 'package:provider/provider.dart';
+  //final pb = Provider.of<PocketBase>(context);
+
+  // Check if the stored credentials are valid
+  //pb.authStore.isValid();
+
+  runApp(
+    Provider<PocketBase>.value(
+      value: pb,
+      child: const Elusiv(),
+    ),
+  );
 }
 
 class Elusiv extends StatelessWidget {
