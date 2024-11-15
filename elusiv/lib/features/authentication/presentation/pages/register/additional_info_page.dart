633126761ad528f7c6b91cdcd07248d8b28bfe2b
K3LV0N <kylenorman44@gmail.com>
2024-11-15 16:17:13 -0500

    Fixed and changed some parts of the themed widgets. Should not effect default useage. Added more cutomizability to themed widgets.

--- elusiv/lib/features/authentication/presentation/pages/register/additional_info_page.dart
+++ elusiv/lib/features/authentication/presentation/pages/register/additional_info_page.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:elusiv/core/common_widgets/test_widgets/go_named_back_button_testing.dart';
 import 'package:elusiv/core/common_widgets/themed_text_field.dart';
 import 'package:elusiv/core/localization/string_extensions.dart';
 import 'package:elusiv/core/navigation/routing.dart';
